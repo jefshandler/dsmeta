4ec9e1262d920b21fab7603a263445ccfb89d8c6
jefshandler <jef_shandler@msn.com>
2022-11-15 19:00:14 -0300

    Segundo Componet

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,11 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from "../../assets/img/notification-icon.svg";
+import "./styles.css";
 function NotificationButton() {
-    return (
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar" />
-        </div>
-    )
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
+    </div>
+  );
 }
 
-export default NotificationButton
+export default NotificationButton;
