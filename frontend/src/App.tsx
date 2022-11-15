f71d28c199b1f0a5b75502de30bd150d9bd18b33
jefshandler <jef_shandler@msn.com>
2022-11-15 13:49:17 -0300

    First Componet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
 function App() {
   return (
-    <h1>Olá Jeferson bem Vindo ao Mundo</h1>
-  )
+    <>
+      <h1>Olá Jeferson bem Vindo ao Mundo</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App
