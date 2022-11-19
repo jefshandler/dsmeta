2b8c674147c68a50863a1a2700b0208cb3d858b9
jefshandler <jef_shandler@msn.com>
2022-11-18 22:24:34 -0300

    Date select

---
+++ backend/src/main/java/com/shandler/dsmeta/services/SalesService.java
@@ -0,0 +1,33 @@
+package com.shandler.dsmeta.services;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.shandler.dsmeta.entities.Sale;
+import com.shandler.dsmeta.repositories.SaleRepository;
+
+
+@Service
+public class SalesService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
+	}
+}
